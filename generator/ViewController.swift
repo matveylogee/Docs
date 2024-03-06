a255709a13e135ca8cb416a0a7693fac864ba6ef
matvey5 <salam121asd@gmail.com>
2024-03-06 18:16:21 +0300

    Initial Commit

---
+++ generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  generator
+//
+//  Created by Матвей on 06.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
